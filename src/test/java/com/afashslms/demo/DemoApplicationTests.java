41f84b8932c883ec883859b8c8f4333df8ac9d7a
dob-by <yeonhwalim03@gmail.com>
2025-03-26 09:22:55 +0900

    Initial commit

---
+++ src/test/java/com/afashslms/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afashslms.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
