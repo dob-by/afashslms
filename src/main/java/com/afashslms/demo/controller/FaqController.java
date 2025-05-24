2cfcbf55372a3ced389d270195d35998e43c248f
dob-by <yeonhwalim03@gmail.com>
2025-05-24 16:20:36 +0900

    feat: 게시판 페이징 + 검색기능 구현

---
+++ src/main/java/com/afashslms/demo/controller/FaqController.java
@@ -0,0 +1,13 @@
+package com.afashslms.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FaqController {
+
+    @GetMapping("/faq")
+    public String faqPage() {
+        return "faq";  // templates/faq.html
+    }
+}
